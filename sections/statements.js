58c214d77fec64a95fc389799820eecb727ff2cb
John Garcia <johngarcia9110@gmail.com>
2016-09-11 19:05:12 -0400

    added in challenge folder and test.js challenge

---
+++ sections/statements.js
@@ -0,0 +1,29 @@
+// Statements.js
+
+// Definition: A group of words, numbers and operators that perform a specific task. 
+
+// Example:
+
+// a = b * 2;
+
+// Most statements end with a semi-colon but it is not always required or needed. 
+// In the example statement "a" and "b" are variables, which are like buckets that can be used to put stuff in,
+// variables hold values to be used by the program. They are symbolic placeholders.
+
+// The "2" in the example is a value, refered to as a literal value (it literally means 2).  Literally values
+// stand alone without being stored in a variable.
+
+// The "=" and "*" characters are OPERATORS. They perform actions with the values and varibales such as assignment 
+// and mathmatic operations.
+
+// Statements are evaluated from the right to the left. The example code tells the computer to get the value stored 
+// in variable "b" and multiply it by 2, then store the result back into another variable we call "a".
+
+
+// The example is not a complete statement as there is no value assigned to "b" thus, the computer cannot
+// complete the statement.
+
+
+
+
+
