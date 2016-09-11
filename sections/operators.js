d69c4a7d242cb4228d26ff8d08affa31cd297884
John Garcia <johngarcia9110@gmail.com>
2016-09-11 15:59:44 +0000

    added operators section

---
+++ sections/operators.js
@@ -0,0 +1,26 @@
+// Basic Operators
+
+// Assignment: = 
+// Math: +, - , * , /
+// Compound Assignment: +=, -=, *=, /=
+// Increment/Decrement: ++, --
+// Property Access: . ( example: console.log() )
+// Equality: == (loose-equals), === (strict-equals), !== (strict not-equals)
+// Comparison: <, >, <=, >=
+// Logical: && (and), || (or)
+
+
+var a = 0;
+
+a = a + 2; // this is the same as..
+
+console.log(a); //2
+
+var a = 0;
+
+a += 2; //..this shortcute operator.
+
+console.log(a);//2
+
+// Both versions of the expression do the same thing, which one you should use is subjective.
+
