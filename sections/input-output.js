a3a74570aa4286e901c358fe8a49a3aeebe34e21
John Garcia <johngarcia9110@gmail.com>
2016-09-11 15:45:01 +0000

    added file for basic input and output. Added index.html for testing purposes

---
+++ sections/input-output.js
@@ -0,0 +1,17 @@
+// Basic Input and Outputs
+
+// if using the console in the browser, the browser will show undefined for any statement that does not have a return value.
+
+console.log("Hello World");// outputs a string
+
+var a = 2;
+console.log(a); // outputs the value of the variable.
+
+//using prompt to accept inputs
+
+//input
+var age = prompt("what is your age?");//always returns a string.
+
+//output
+console.log(age);
+
